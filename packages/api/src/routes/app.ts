c437b5fd1487ac87f6fee5f24a3ad918d5937205
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-24 23:37:45 +0200

    chore: Refactor app route to use createRequire for module resolution and update appDist path

--- packages/api/src/routes/app.ts
+++ packages/api/src/routes/app.ts
@@ -1,10 +1,12 @@
 import type { FastifyInstance } from 'fastify';
+import { createRequire } from 'node:module';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import fastifyStatic from '@fastify/static';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const appDist = path.resolve(__dirname, '../../../app/dist');
+const require = createRequire(import.meta.url);
+const appDist = path.join(
+  path.dirname(require.resolve('@maxigarcia/mockingbird-app')),
+);
 
 export function initAppRoutes(fastify: FastifyInstance) {
   fastify.register(fastifyStatic, {
