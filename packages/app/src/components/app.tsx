4dd642493a7897ded44c3424372a25962a20346d
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-26 15:53:30 +0200

    refactor: Replace individual components in App with a new RequestForm component for better organization

--- packages/app/src/components/app.tsx
+++ packages/app/src/components/app.tsx
@@ -1,19 +1,11 @@
 import { Aside } from './aside';
-import { OverridesPanel } from './overrides-panel';
-import { RequestEditor } from './request-editor';
-import { SaveRequestButton } from './save-request-button';
+import { RequestForm } from './request-form';
 
 export function App() {
   return (
     <>
       <Aside />
-      <div className="flex flex-col gap-4 py-2 pr-4">
-        <RequestEditor />
-        <OverridesPanel />
-        <div className="flex justify-end">
-          <SaveRequestButton className="w-full max-w-32" />
-        </div>
-      </div>
+      <RequestForm />
     </>
   );
 }
---
+++ packages/app/src/components/request-form.tsx
@@ -0,0 +1,15 @@
+import { OverridesPanel } from './overrides-panel';
+import { RequestEditor } from './request-editor';
+import { SaveRequestButton } from './save-request-button';
+
+export function RequestForm() {
+  return (
+    <div className="flex flex-col gap-4 py-2 pr-4">
+      <RequestEditor />
+      <OverridesPanel />
+      <div className="flex justify-end">
+        <SaveRequestButton className="w-full max-w-32" />
+      </div>
+    </div>
+  );
+}
