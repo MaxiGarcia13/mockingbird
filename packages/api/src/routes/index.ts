e3c938988b4b159edfc1e481ce86df03a29b306f
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-26 14:29:16 +0200

    chore: Refactor routes to remove storage and integrate CORS support

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -1,11 +1,17 @@
 import type { FastifyInstance } from 'fastify';
+import cors from '@fastify/cors';
 import { initAppRoutes } from './app.js';
-import { initStorageRoutes } from './storage.js';
+import { initRequestsRoutes } from './requests.js';
 
 export function initRoutes(fastify: FastifyInstance) {
-  initAppRoutes(fastify);
+  fastify.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  });
 
-  initStorageRoutes(fastify);
+  initAppRoutes(fastify);
+  initRequestsRoutes(fastify);
 
   fastify.setNotFoundHandler((_request, reply) => {
     reply.sendFile('index.html');
--- packages/api/src/routes/storage.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-
-export function initStorageRoutes(fastify: FastifyInstance) {
-  fastify.get('/storage', (_request, reply) => {
-    reply.send({ message: 'Hello, world!' });
-  });
-}
