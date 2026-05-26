a21cacbb7e3f5fece21fb5840915f60c550e57e1
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-26 16:19:48 +0200

    feat: Enhance Aside component to include SavedRequestsList and header for better user experience

--- packages/app/src/components/aside.tsx
+++ packages/app/src/components/aside.tsx
@@ -1,9 +1,13 @@
 import { Brand } from './brand';
+import { SavedRequestsList } from './saved-requests-list';
 
 export function Aside() {
   return (
-    <aside className="bg-surface">
+    <aside className="flex h-full flex-col gap-4 overflow-y-auto bg-surface py-2">
       <Brand />
+
+      <h2 className="px-4 text-sm font-medium">Saved requests</h2>
+      <SavedRequestsList />
     </aside>
   );
 }
