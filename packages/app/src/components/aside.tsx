459e076826d42b4d2353675d472b3ed3a337e0df
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-26 18:21:21 +0200

    feat: Update Aside component to include NewRequestButton and improve layout of saved requests section

--- packages/app/src/components/aside.tsx
+++ packages/app/src/components/aside.tsx
@@ -1,12 +1,15 @@
 import { Brand } from './brand';
+import { NewRequestButton } from './new-request-button';
 import { SavedRequestsList } from './saved-requests';
 
 export function Aside() {
   return (
     <aside className="flex h-full flex-col gap-4 overflow-y-auto bg-surface py-2">
       <Brand />
-
-      <h2 className="px-4 text-sm font-medium">Saved requests</h2>
+      <div className="flex flex-wrap items-center justify-between gap-2 px-4">
+        <h2 className="text-sm font-medium">Saved requests</h2>
+        <NewRequestButton />
+      </div>
       <SavedRequestsList />
     </aside>
   );
