607a1283a3a023e4c56a3e787da9c66a13d47932
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-29 12:44:28 +0200

    feat: Integrate RequestEditor and enhance App layout
    - Added the RequestEditor component to the App, utilizing the new useRequestFormStore for state management.
    - Updated the App layout with improved styling and structure, including a dedicated section for the RequestEditor.
    - Introduced a new store module for managing request form state, enhancing modularity and reusability.

--- packages/chromium-extension/src/components/app.tsx
+++ packages/chromium-extension/src/components/app.tsx
@@ -1,18 +1,23 @@
 import { AppDescription } from '@maxigarcia/mockingbird-shared/components/app-description';
 import { Brand } from '@maxigarcia/mockingbird-shared/components/brand';
 import { GithubLink } from '@maxigarcia/mockingbird-shared/components/github-link';
+import { RequestEditor } from '@maxigarcia/mockingbird-shared/components/request-editor/request-editor';
+import { useRequestFormStore } from '@/store/request-form';
 import { version } from '../../package.json';
 
 export function App() {
   return (
     <>
-      <header className="space-y-4 px-4 py-2">
+      <header className="space-y-4 p-4">
         <div className="flex items-center gap-4">
           <Brand />
           <GithubLink version={version} />
         </div>
         <AppDescription />
       </header>
+      <section className="p-4">
+        <RequestEditor storeFn={useRequestFormStore} />
+      </section>
     </>
   );
 }
